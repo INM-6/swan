876aa3c381635676d3ab96478efcc581e5f3b13e
Christoph Gollan <c.gollan@fz-juelich.de>
2015-10-13 09:00:31 +0200

    Changed link locations for SIP and PyQt

--- install_pyqt.sh
+++ install_pyqt.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 cd ..
-curl -O "http://downloads.sourceforge.net/project/pyqt/sip/sip-4.16.9/sip-4.16.9.tar.gz"
+curl -O http://kent.dl.sourceforge.net/project/pyqt/sip/sip-4.16.9/sip-4.16.9.tar.gz
 tar -xvf sip-4.16.9.tar.gz
 cd sip-4.16.9
 python configure.py
 make
 sudo make install
 cd ..
-curl -O "http://downloads.sourceforge.net/project/pyqt/PyQt4/PyQt-4.11.4/PyQt-x11-gpl-4.11.4.tar.gz"
+curl -O http://skylink.dl.sourceforge.net/project/pyqt/PyQt4/PyQt-4.11.4/PyQt-x11-gpl-4.11.4.tar.gz
 tar -xvf PyQt-x11-gpl-4.11.4.tar.gz
 cd PyQt-x11-gpl-4.11.4
 python configure.py --confirm-license
