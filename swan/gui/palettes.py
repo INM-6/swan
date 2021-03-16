24f7a7e5f75c8f1d06bb5aafa2d15eb1cb10a961
Shashwat Sridhar <shashwat.sridhar.95@gmail.com>
2021-03-16 15:58:34 +0100

    replace pg.QtGui with PyQt5.QtGui

--- swan/gui/palettes.py
+++ swan/gui/palettes.py
@@ -2,11 +2,11 @@
 @author: Shashwat Sridhar
 """
 import numpy as np
-import pyqtgraph as pg
+from PyQt5 import QtGui
 import colorcet
 
 
-class DarkPalette(pg.QtGui.QPalette):
+class DarkPalette(QtGui.QPalette):
     """Class that inherits from pyqtgraph.QtGui.QPalette and renders dark colours for the application."""
 
     def __init__(self):
@@ -14,19 +14,19 @@ class DarkPalette(pg.QtGui.QPalette):
         self.setup()
 
     def setup(self):
-        self.setColor(pg.QtGui.QPalette.Window, pg.QtGui.QColor(53, 50, 47))
-        self.setColor(pg.QtGui.QPalette.WindowText, pg.QtGui.QColor(255, 255, 255))
-        self.setColor(pg.QtGui.QPalette.Base, pg.QtGui.QColor(30, 27, 24))
-        self.setColor(pg.QtGui.QPalette.AlternateBase, pg.QtGui.QColor(53, 50, 47))
-        self.setColor(pg.QtGui.QPalette.ToolTipBase, pg.QtGui.QColor(255, 255, 255))
-        self.setColor(pg.QtGui.QPalette.ToolTipText, pg.QtGui.QColor(255, 255, 255))
-        self.setColor(pg.QtGui.QPalette.Text, pg.QtGui.QColor(255, 255, 255))
-        self.setColor(pg.QtGui.QPalette.Button, pg.QtGui.QColor(53, 50, 47))
-        self.setColor(pg.QtGui.QPalette.ButtonText, pg.QtGui.QColor(255, 255, 255))
-        self.setColor(pg.QtGui.QPalette.BrightText, pg.QtGui.QColor(255, 0, 0))
-        self.setColor(pg.QtGui.QPalette.Link, pg.QtGui.QColor(42, 130, 218))
-        self.setColor(pg.QtGui.QPalette.Highlight, pg.QtGui.QColor(42, 130, 218))
-        self.setColor(pg.QtGui.QPalette.HighlightedText, pg.QtGui.QColor(0, 0, 0))
+        self.setColor(QtGui.QPalette.Window, QtGui.QColor(53, 50, 47))
+        self.setColor(QtGui.QPalette.WindowText, QtGui.QColor(255, 255, 255))
+        self.setColor(QtGui.QPalette.Base, QtGui.QColor(30, 27, 24))
+        self.setColor(QtGui.QPalette.AlternateBase, QtGui.QColor(53, 50, 47))
+        self.setColor(QtGui.QPalette.ToolTipBase, QtGui.QColor(255, 255, 255))
+        self.setColor(QtGui.QPalette.ToolTipText, QtGui.QColor(255, 255, 255))
+        self.setColor(QtGui.QPalette.Text, QtGui.QColor(255, 255, 255))
+        self.setColor(QtGui.QPalette.Button, QtGui.QColor(53, 50, 47))
+        self.setColor(QtGui.QPalette.ButtonText, QtGui.QColor(255, 255, 255))
+        self.setColor(QtGui.QPalette.BrightText, QtGui.QColor(255, 0, 0))
+        self.setColor(QtGui.QPalette.Link, QtGui.QColor(42, 130, 218))
+        self.setColor(QtGui.QPalette.Highlight, QtGui.QColor(42, 130, 218))
+        self.setColor(QtGui.QPalette.HighlightedText, QtGui.QColor(0, 0, 0))
 
 
 UNIT_COLORS = (np.array(colorcet.glasbey_bw_minc_20_minl_30) * 255).astype(np.int32)
