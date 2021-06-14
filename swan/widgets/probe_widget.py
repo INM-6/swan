2b33c5f59dbf0bd59b3fc4485006c670c581323f
okloss <o.kloss@fz-juelich.de>
2021-06-14 09:36:00 +0200

    saving changes to select_channel method to test on different device

--- swan/widgets/probe_widget.py
+++ swan/widgets/probe_widget.py
@@ -13,53 +13,53 @@ class ProbeWidget(QtWidgets.QWidget):
 
     def __init__(self, *args, **kwargs):
         super(ProbeWidget, self).__init__(*args, **kwargs)
-
-
         layout=QtWidgets.QGridLayout()
 
+        self.coordinates = []
+        self.dirty_channels = []
+        self.lastcannel = 1
+        self.currentchannel = 1
 
-        self.chosenCoordsArr = []
-        self.chosenCoordsLi = []
-        self.standardCoordsLi = []
         for i in range(1, 11):
             for j in range(1, 11):
-                self.standardCoordsLi.append((i, j))
-        self.standardCoordsArr = numpy.array(self.standardCoordsLi)
+                self.coordinates.append([i, j])
 
         self.graphWidget = pg.PlotWidget(parent=self)
         layout.addWidget(self.graphWidget)
         self.setLayout(layout)
 
         # plot data: x, y values
-        self.plot_points(self.standardCoordsArr)
+        self.plot_points(numpy.array(self.coordinates))
         self.graphWidget.hideAxis('left')
         self.graphWidget.hideAxis('bottom')
 
-    def plot_points(self, Coords):
-        scatterplot = pg.ScatterPlotItem(pos = Coords)
+    def plot_points(self, coords):
+        scatterplot = pg.ScatterPlotItem(pos = coords)
         scatterplot.sigClicked.connect(self.points_clicked)
         self.graphWidget.addItem(scatterplot)
 
-    def points_clicked(self, item, points, ev):
+    def points_clicked(self, item,  points, ev):
         point = points[0]
         x, y = point.pos().x(), point.pos().y()
-        if [x, y] in self.standardCoordsArr:
-            print(self.standardCoordsLi.index((x, y)))
-        elif [x, y] in self.chosenCoordsArr:
-            print(self.chosenCoordsLi.index([x, y]))
-        else: raise IndexError
+        try:
+            print(self.coordinates.index([x, y]))
+            self.dirty_channels.append(self.coordinates.index([x, y]))
+            self.select_channel(self, self.coordinates.index([x, y]))
+        except ValueError:
+            print('couldnt be found')
         ev.accept()
 
-    def update(self, filename):
+    def load_geometry(self, filename): #bsp load_geometry
         if filename.endswith('prb'):
             probe = read_prb(filename)
         elif filename.endswith('json'):
             probe = read_probeinterface(filename)
         else: raise ValueError
         self.graphWidget.clear()
+        self.coordinates = probe.probes[0].contact_positions.tolist()
         self.chosenCoordsLi = probe.probes[0].contact_positions.tolist()
         self.chosenCoordsArr = probe.probes[0].contact_positions
-        self.plot_points(self.chosenCoordsArr)
+        self.plot_points(numpy.array(self.coordinates))
 
 
     def make_grid(self, rows=10, cols=10):
@@ -120,7 +120,11 @@ class ProbeWidget(QtWidgets.QWidget):
                 The channel id of the item.
 
         """
-        pass
+        self.lastchannel = self.currentchannel
+        self.currentchannel = channel
+        lastchannel = self.lastchannel
+        self.doChannel.emit(channel, lastchannel)
+
 
 
 
